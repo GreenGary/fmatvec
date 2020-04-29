ceda679d8bc4d3121364b9eccd3079e379cffd51
Robert Kühberger <robert@kuehberger.consulting>
2020-04-29 17:00:09 +0200

    Use managed C++ objects

--- c#/EigvalClr/EigvalClrClass.cs
+++ c#/EigvalClr/EigvalClrClass.cs
@@ -12,14 +12,30 @@ namespace EigvalClr
     {
         static void Main(string[] args)
         {
-
-            LinearAlgebraDouble linalg = new LinearAlgebraDouble();
-            double[] squareMatrix = { 2.0, 30.0, 20.0, 4.0  };
-            double[] eigValVec = linalg.eigval(2, squareMatrix);
+            // Beispiel mit CLR arrays and statischer Methode
+            double[] matrix = { 2.0, 30.0, 20.0, 4.0  };
+            double[] eigValVec = LinearAlgebraDouble.eigval(2, matrix);
             foreach (double ev in eigValVec)
             {
                 Console.WriteLine(ev);
             }
+
+            // Beispiel mit Matrix Klasse und array return
+            SquareMatrix sm = new SquareMatrix(2, matrix);
+            eigValVec = sm.eigval();
+            foreach (double ev in eigValVec)
+            {
+                Console.WriteLine(ev);
+            }
+
+            // Beispiel mit Matrix Klasse und ComplexVac return
+            ComplexVec complexVec = sm.eigvalComplexVec();
+            for(int i = 0; i < complexVec.Size; ++i)
+            {
+                Console.WriteLine(complexVec.e(i));
+            }
+
+
         }
     }
 }
